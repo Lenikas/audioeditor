4a1fc51ba3b5e32a1b0fec29493d771ae187969f
Lenikas <stuffer232@gmail.com>
2019-05-07 22:14:32 +0500

    Консольная закончена,работа с tkinter

--- GUI.py
+++ GUI.py
@@ -1,18 +1,57 @@
 from tkinter import *
 from tkinter import filedialog, constants
 from editor import FunctionForWav
+import struct
 
+
+def explorer():
+    return filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("wav files","*.wav"),("all files","*.*")))
+
+
+def save_file():
+    file = explorer()
+    with open(file, "rb") as file:
+        data = file.read()
+    return data
+
+
+def cut_audio(how_much):
+    """Записывает в новый файл часть входящего файла
+    Отрезает от входящего аудио какую-то часть(работает пока только с целыми числами)
+    и записывает в новый файл.На выходе получаем новое аудио, являющееся отрезком входящего
+    """
+    data = FunctionForWav(save_file())
+    how_much = int(how_much)
+    size = data.extract_size()
+    new_size = size // 2
+    result = b'RIFF'
+    result += struct.pack('>L', new_size)
+    result += data[8:40]
+    bytes_region = data.extract_how_much_in_dataregion()
+    new_bytes_region = bytes_region // 2
+    result += struct.pack('>L', new_bytes_region)
+    data_wav = data[44:]
+    result += data_wav[44: len(data_wav) // 2]
+    with open("Cut", "wb") as file:
+        file.write(result)
+    return result
 
 
 root = Tk()
+root["bg"] = "#FFFFFF"
 root.title("Audio Editor")
 root.wm_state("zoomed")
-root.filename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("wav files","*.wav"),("all files","*.*")))
-#root.filename1 = filedialog.asksaveasfilename(initialdir = "/",title = "Select file",filetypes = (("wav files","*.wav"),("all files","*.*")))
-with open(root.filename, 'rb') as file:
-    data= FunctionForWav(file.read())
-btn_cut = Button(text="Резка", bg="#FF4500", fg="#000000", padx="10", command=data.cut_audio(2))
-btn_cut.place(relx=.5, rely=.99, anchor="c", bordermode=OUTSIDE)
 
+btn_expl = Button(text="Обзор", bg="#B0E0E6", fg="#000000", padx="10", width = 8,  command=save_file)
+btn_expl.place(relx=.488, rely=.99, anchor="c", bordermode=OUTSIDE)
 
+btn_cut = Button(text="Резка", bg="#B0E0E6", fg="#000000", padx="10", width = 8, command=cut_audio)
+btn_cut.place(relx=.54, rely=.99, anchor="c", bordermode=OUTSIDE)
+
+btn_splice = Button(text="Склейка", bg="#B0E0E6", fg="#000000", padx="10", width = 8)
+btn_splice.place(relx=.59, rely=.99, anchor="c", bordermode=OUTSIDE)
 root.mainloop()
+
+
+
+
--- test.py
+++ test.py
@@ -1,8 +1,15 @@
 import unittest
+import os
 from editor import *
 
 class TestDataMethods(unittest.TestCase):
 
 
     def test_size(self):
-        pass
+        with open(r"C:\Users\Леня\Desktop\audioeditor\coincidence.wav", "rb") as file:
+            data1 = file.read()
+        size_os = os.path.getsize(r"C:\Users\Леня\Desktop\audioeditor\coincidence.wav")
+        self.assertEqual(FunctionForWav(data1).extract_size(), size_os)
+
+if __name__ == '__main__':
+    unittest.main()
